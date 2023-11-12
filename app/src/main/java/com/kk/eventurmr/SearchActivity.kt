9c0d377f2674c9af8ba1cae8be7e6d82daf261b5
tyukei <0024481294bxyk6037@gmail.com>
2023-11-12 11:50:49 +0900

    add: search

--- app/src/main/java/com/kk/eventurmr/SearchActivity.kt
+++ app/src/main/java/com/kk/eventurmr/SearchActivity.kt
@@ -1,19 +1,27 @@
 package com.kk.eventurmr
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.ListView
-import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : BaseActivity() {
 
     private lateinit var searchEditText: EditText
     private lateinit var searchResultsListView: ListView
+    private lateinit var adapter: ArrayAdapter<String>
+
+    // Original full list of items
+    private val allItems = arrayListOf("Apple", "Banana", "Cherry", "Date", "Elderberry", "Fig", "Grape")
+    // List for filtered items
+    private val filteredItems = ArrayList<String>(allItems)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search) // Replace with your actual layout resource name
+        setContentView(R.layout.activity_search)
 
         searchEditText = findViewById(R.id.searchEditText)
         searchResultsListView = findViewById(R.id.searchResultsListView)
@@ -25,25 +33,29 @@ class SearchActivity : BaseActivity() {
     }
 
     private fun setupSearchEditText() {
-        searchEditText.setOnEditorActionListener { textView, i, keyEvent ->
-            // Handle search action here
-            // For example, query a database or a server with textView.text.toString()
-            true // Return true to indicate you have handled the event
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                performSearch(s.toString().trim())
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+    }
+
+    private fun performSearch(query: String) {
+        filteredItems.clear()
+        if (query.isEmpty()) {
+            filteredItems.addAll(allItems)
+        } else {
+            val searchResults = allItems.filter { it.contains(query, ignoreCase = true) }
+            filteredItems.addAll(searchResults)
         }
+        adapter.notifyDataSetChanged()
     }
 
     private fun setupSearchResultsListView() {
-        // Placeholder data for the ListView
-        val searchResults = arrayOf("Result 1", "Result 2", "Result 3") // Replace with real search results
-
-        // Create an ArrayAdapter using the default list item layout and the search results
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, searchResults)
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, filteredItems)
         searchResultsListView.adapter = adapter
-
-        // Set up the item click listener for the ListView
-        searchResultsListView.setOnItemClickListener { _, _, position, _ ->
-            // You can handle the search result click here
-            // For example, you might want to open a new Activity with the result details
-        }
     }
 }
