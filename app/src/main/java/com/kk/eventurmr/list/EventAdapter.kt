7251a1ee9b3577e7c7217849e2855909a4bb7c98
tyukei <0024481294bxyk6037@gmail.com>
2023-11-20 18:00:20 +0900

    update: data

--- app/src/main/java/com/kk/eventurmr/list/EventAdapter.kt
+++ app/src/main/java/com/kk/eventurmr/list/EventAdapter.kt
@@ -3,6 +3,7 @@ package com.kk.eventurmr.list
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.text.TextUtils.substring
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,10 +24,11 @@ class EventAdapter(context: Context, events: List<Event>) :
         val view =
             convertView ?: LayoutInflater.from(context).inflate(R.layout.item_list, parent, false)
         val date = TimeUtil.getDateString(event?.dateTime)
-
+        val lastSpaceIndex = event?.description?.substring(0, 30)?.lastIndexOf(' ')
 
         view.findViewById<TextView>(R.id.eventTitle).text = event?.name
-        view.findViewById<TextView>(R.id.eventDescription).text = event?.description
+        view.findViewById<TextView>(R.id.eventDescription).text =
+            lastSpaceIndex?.let { event?.description?.substring(0, it) }
         view.findViewById<TextView>(R.id.eventDate).text = date
         view.findViewById<ImageView>(R.id.eventMap)
 
