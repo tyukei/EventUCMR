f68f1c49dd5da2903b75f269d79a1a45f500f81c
tyukei <0024481294bxyk6037@gmail.com>
2023-11-22 18:10:24 +0900

    update: date format

--- app/src/main/java/com/kk/data/TimeUtil.kt
+++ app/src/main/java/com/kk/data/TimeUtil.kt
@@ -26,17 +26,36 @@ class TimeUtil {
 
         fun getDateString(time: Int?): String {
             // 23110110 to 2023-11-01 10:00
+            // Nov 01, 2023 (10:00 am)
             val timeString = time.toString()
             val year = timeString.substring(0, 2)
             val month = timeString.substring(2, 4)
             val day = timeString.substring(4, 6)
             val hour = timeString.substring(6, 8)
-            return if (hour.toInt() < 12) {
-                "20$year-$month-$day ($hour:00 am)"
+            var timeHour = ""
+
+            if (hour.toInt() < 12) {
+                timeHour = "($hour:00 am)"
             } else {
                 val newhour = hour.toInt() - 12
-                "20$year-$month-$day ($newhour:00 pm)"
+                timeHour = " ($newhour:00 pm)"
             }
+            val monthString = when (month) {
+                "01" -> "Jan"
+                "02" -> "Feb"
+                "03" -> "Mar"
+                "04" -> "Apr"
+                "05" -> "May"
+                "06" -> "Jun"
+                "07" -> "Jul"
+                "08" -> "Aug"
+                "09" -> "Sep"
+                "10" -> "Oct"
+                "11" -> "Nov"
+                "12" -> "Dec"
+                else -> "Jan"
+            }
+            return "$monthString $day, 20$year $timeHour"
         }
 
         fun getCurrentInt(): Int {
